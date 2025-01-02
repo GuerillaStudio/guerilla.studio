7dd82b3973bfd89776df68603e32b971b3a02358
Tixie <contact@tixie.name>
2025-01-02 19:10:18 +0100

    Add alerts elements to markdown parser
    like on Github: https://docs.github.com/en/get-started/writing-on-github/getting-started-with-writing-and-formatting-on-github/basic-writing-and-formatting-syntax#alerts

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,5 +1,6 @@
 const { DateTime } = require("luxon");
 const markdownItAnchor = require("markdown-it-anchor");
+const markdownItGitHubAlerts = require("markdown-it-github-alerts");
 
 const pluginRss = require("@11ty/eleventy-plugin-rss");
 const pluginSyntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
@@ -90,6 +91,7 @@ module.exports = function(eleventyConfig) {
 			level: [1,2,3,4],
 			slugify: eleventyConfig.getFilter("slugify")
 		});
+		mdLib.use(markdownItGitHubAlerts);
 	});
 
 	// Features to make your build faster (when you need them)
